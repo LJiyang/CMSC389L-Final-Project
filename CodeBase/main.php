b83a80bcc286053013e48aa4b5c95c708677a7f9
Jiyang Li <ljy1995@terpmail.umd.edu>
2018-04-30 01:02:36 -0400

    Create main.php

---
+++ CodeBase/main.php
@@ -0,0 +1,17 @@
+<?php
+    if(isset($_POST['su'])) {
+        header("Location: signup.php");
+    }
+
+    if(isset($_POST['ra'])) {
+        header("Location: reviewApplication.php");
+    }
+
+    if(isset($_POST['s'])) {
+        header("Location: search.php");
+    }
+
+    if(isset($_POST['u'])) {
+        header("Location: update.php");
+    }
+?>
