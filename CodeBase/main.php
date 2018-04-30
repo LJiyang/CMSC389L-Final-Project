934f1f9af55c1a072382b461bc3c030e36109e0f
Jiyang Li <ljy1995@terpmail.umd.edu>
2018-04-30 01:20:40 -0400

    Update main.php

--- CodeBase/main.php
+++ CodeBase/main.php
@@ -2,15 +2,9 @@
     if(isset($_POST['su'])) {
         header("Location: signup.php");
     }
-
-    if(isset($_POST['ra'])) {
-        header("Location: reviewApplication.php");
-    }
-
     if(isset($_POST['s'])) {
         header("Location: search.php");
     }
-
     if(isset($_POST['li'])) {
         header("Location: login.php");
     }
